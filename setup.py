a3fcb426b8781fc671b86052969699add5c02003
ahan98 <ahan98@gmail.com>
2024-01-16 19:52:43 +0000

    update packages for Python 3.8 compatibility

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='code_transformer',
-      version='0.1',
-      description='Code Transformer',
-      author='Daniel Zügner, Tobias Kirschstein, Michele Catasta, Jure Leskovec, Stephan Günnemann',
-      author_email='zuegnerd@in.tum.de,kirschto@in.tum.de',
-      packages=find_packages(),
-      install_requires=['jsonlines==1.2.0', 'rouge==1.0.0', 'joblib==0.14.1', 
-                        'scipy==1.4.1', 'networkx==2.4', 'Pygments==2.6.1', 'torch==1.4.0', 
-                        'numpy==1.18.1', 'jsonpickle==1.3', 'pandas==1.0.5', 'tqdm==4.43.0',
-                        'transformers==3.1.0', 'six==1.14.0', 'pytest==6.2.2', 'PyYAML==5.4.1',
-                        'Requests==2.23.0', 'scikit_learn==0.24.1', 'environs==9.3.1', 'sacred==0.8.1'],
-      zip_safe=False)
+setup(
+    name='code_transformer',
+    version='0.1',
+    description='Code Transformer',
+    author='Daniel Zügner, Tobias Kirschstein, Michele Catasta, Jure Leskovec, Stephan Günnemann',
+    author_email='zuegnerd@in.tum.de,kirschto@in.tum.de',
+    packages=find_packages(),
+    install_requires=[
+        "jsonlines==4.0.0", "rouge==1.0.1", "tensorflow>=2.0.0",
+        "joblib==1.3.2", "scipy==1.10.1", "networkx<3.0.0", "Pygments==2.15.1",
+        "torch==2.1.2", "numpy>=1.17", "jsonpickle==3.0.2", "pandas==2.0.3",
+        "tqdm==4.66.1", "transformers==4.36.2", "six==1.16.0",
+        "pytest==7.4.4", "PyYAML==6.0.1", "Requests==2.31.0",
+        "scikit_learn==1.3.2", "environs==10.3.0", "sacred==0.8.5"
+    ],
+    zip_safe=False
+)
