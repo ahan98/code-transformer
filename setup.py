ea12c385712f1596487ce4e24740ebeea8153784
ahan98 <ahan98@gmail.com>
2024-01-17 13:52:23 +0000

    update packages

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='code_transformer',
     version='0.1',
@@ -7,13 +10,6 @@ setup(
     author='Daniel Zügner, Tobias Kirschstein, Michele Catasta, Jure Leskovec, Stephan Günnemann',
     author_email='zuegnerd@in.tum.de,kirschto@in.tum.de',
     packages=find_packages(),
-    install_requires=[
-        "jsonlines==4.0.0", "rouge==1.0.1", "tensorflow>=2.0.0",
-        "joblib==1.3.2", "scipy==1.10.1", "networkx<3.0.0", "Pygments==2.15.1",
-        "torch==2.1.2", "numpy>=1.17", "jsonpickle==3.0.2", "pandas==2.0.3",
-        "tqdm==4.66.1", "transformers==4.36.2", "six==1.16.0",
-        "pytest==7.4.4", "PyYAML==6.0.1", "Requests==2.31.0",
-        "scikit_learn==1.3.2", "environs==10.3.0", "sacred==0.8.5"
-    ],
-    zip_safe=False
+    install_requires=requirements,
+    zip_safe=False,
 )
